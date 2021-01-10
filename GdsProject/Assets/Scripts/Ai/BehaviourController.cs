9d4fc8f6c0ff4c6763bd96b2932583f874030f90
Risist <risistt@gmail.com>
2021-01-10 18:04:28 +0100

    ai and health systems

---
+++ GdsProject/Assets/Scripts/Ai/BehaviourController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ai;
+
+namespace Ai
+{
+    public class BehaviourController : MonoBehaviour
+    {
+        #region References
+
+        [NonSerialized] public readonly StateMachine stateMachine = new StateMachine();
+
+        #endregion References
+
+
+        #region Blackboard
+
+        readonly GenericBlackboard _blackboard = new GenericBlackboard();
+
+        public BoxValue<T> InitBlackboardValue<T>(string key, Func<T> initializeMethod)
+        {
+            return _blackboard.InitValue<T>(key, initializeMethod);
+        }
+
+        public BoxValue<T> InitBlackboardValue<T>(string key) where T : new()
+        {
+            return _blackboard.InitValue<T>(key);
+        }
+
+        public BoxValue<T> GetBlackboardValue<T>(string key)
+        {
+            return _blackboard.GetValue<T>(key);
+        }
+
+        #endregion Blackboard
+
+        #region BehaviourPack
+
+        [Tooltip("Behaviour packs are used to insert groups of behaviours to this agent")] [SerializeField]
+        BehaviourPack[] _behaviourPacks = new BehaviourPack[0];
+
+        void InitBehaviourPacks()
+        {
+            foreach (var it in _behaviourPacks)
+                it.DefineBehaviours(this);
+        }
+
+        #endregion BehaviourPack
+
+        private void Start()
+        {
+            InitBehaviourPacks();
+            Debug.Assert(stateMachine.currentState != null, "No behaviour pack initialized current state");
+        }
+
+        private void FixedUpdate()
+        {
+            stateMachine.UpdateStates();
+        }
+    }
+
+}
---
+++ GdsProject/Assets/Scripts/Ai/BehaviourPack/BehaviourPack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ai
+{
+    public abstract class BehaviourPack : ScriptableObject
+    {
+        public void DefineBehaviours(BehaviourController controller)
+        {
+            this.controller = controller;
+            DefineBehaviours_Impl();
+        }
+        protected abstract void DefineBehaviours_Impl();
+
+        protected BehaviourController controller;
+
+        ////////// Utility
+        ///
+
+        public const string enemyId = nameof(enemyId);
+        public const string allyId = nameof(allyId);
+        public const string neutralId = nameof(neutralId);
+        public const string noiseId = nameof(noiseId);
+        public const string painId = nameof(painId);
+        public const string touchId = nameof(touchId);
+
+    }
+}
