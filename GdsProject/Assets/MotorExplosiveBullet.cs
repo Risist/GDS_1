a6e83024b33478a47512d0f124a555f319b49a46
Risist <risistt@gmail.com>
2021-01-26 19:15:34 +0100

    ds

--- GdsProject/Assets/MotorExplosiveBullet.cs
+++ GdsProject/Assets/MotorExplosiveBullet.cs
@@ -29,21 +29,21 @@ public class MotorExplosiveBullet : MonoBehaviour
         _target = ObstacleMarker.FindClosestFreePosition(_target, requiredFreeArea, freeAreaTestOffset);
     }
 
+    public void OnGroundHit(float x)
+    {
+        Vector3 spawnPos = transform.position;
+        spawnPos.y = _target.y + explosionCreationOffset;
+
+        Instantiate(holePrefab, spawnPos, Quaternion.identity);
+        foreach (var item in spawnedObjectsOnExplosion)
+        {
+            Instantiate(item);
+        }
+        Destroy(gameObject);
+    }
+
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, _target, speed * Time.deltaTime);
-
-        if (transform.position.y < _target.y + explosionCreationOffset)
-        {
-            Vector3 spawnPos = transform.position;
-            spawnPos.y = _target.y + explosionCreationOffset;
-
-            Instantiate(holePrefab, spawnPos, Quaternion.identity);
-            foreach (var item in spawnedObjectsOnExplosion)
-            {
-                Instantiate(item);
-            }
-            Destroy(gameObject);
-        }
     }
 }
